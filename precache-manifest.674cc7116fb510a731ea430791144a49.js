24d85a5a9317ecb2b39269499e247f3b2d8e87d5
Gukhwa Jeon <g.jeon3743@gmail.com>
2019-10-05 17:29:05 +0200

    Updates

---
+++ precache-manifest.674cc7116fb510a731ea430791144a49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6482950a8633ac25612a64537955b9",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "24491c0f089f4596eaea",
+    "url": "/movie_app_2019/static/css/main.a8f13ffa.chunk.css"
+  },
+  {
+    "revision": "db47374aee0b75199910",
+    "url": "/movie_app_2019/static/js/2.492ac49c.chunk.js"
+  },
+  {
+    "revision": "24491c0f089f4596eaea",
+    "url": "/movie_app_2019/static/js/main.d7284d00.chunk.js"
+  },
+  {
+    "revision": "5d5be9211293a7d3f578",
+    "url": "/movie_app_2019/static/js/runtime-main.63eba884.js"
+  }
+]);
